ae88199d0549691e620a2230e8ba9c4f921c761a
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-09-21 11:56:28 -0700

    Handled ValueError

--- average_calculator/calc.py
+++ average_calculator/calc.py
@@ -12,9 +12,12 @@ def calculator():
         user_input = input("Enter an integer or 'compute': ")
         
         if user_input == "compute":
-            print_average(numbers)
-            finished = True
-           
+            try:
+                print_average(numbers)
+                finished = True
+            except ValueError:
+                print("You must enter at least one number before calculating an average.")
+            
         else:
             try:
                 number = int(user_input)
